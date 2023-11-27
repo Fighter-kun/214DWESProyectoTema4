58db170314104a500b5cbd93143aedf3cadfb104
Fighter-kun <carlosgarciacachon@gmail.com>
2023-11-27 18:57:01 +0100

    UPDATE - Corregido Script CargaInicial de Explotación

--- scriptDB/CargaInicialDAW214DBDepartamentos.sql
+++ scriptDB/CargaInicialDAW214DBDepartamentos.sql
@@ -8,7 +8,7 @@ USE DB214DWESProyectoTema4;
 
 -- Inserto los datos iniciales en la tabla Departamento
 INSERT INTO T02_Departamento (T02_CodDepartamento, T02_DescDepartamento, T02_FechaCreacionDepartamento, T02_VolumenDeNegocio, T02_FechaBajaDepartamento) VALUES
-    ('AAA', 'Departamento de Ventas', '2023-11-13 13:06:00', 100000.50, NULL),
-    ('AAB', 'Departamento de Marketing', '2023-11-13 13:06:00', 50089.50, NULL),
-    ('AAC', 'Departamento de Finanzas', '2022-11-13 13:06:00', 600.50, '2023-11-13 13:06:00');
+    ('AAA', 'Departamento de Ventas', NOW(), 100000.50, NULL),
+    ('AAB', 'Departamento de Marketing', NOW(), 50089.50, NULL),
+    ('AAC', 'Departamento de Finanzas', NOW(), 600.50, '2023-11-13 13:06:00');
 
